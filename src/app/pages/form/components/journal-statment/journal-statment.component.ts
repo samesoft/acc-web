81dec914754dcc41ed2ee64c65d3ba1d655b5697
mahletkidanemariam <mahletkidanemariam14@gmail.com>
2024-07-02 21:14:56 +0300

    journal edit

--- src/app/pages/form/components/journal-statment/journal-statment.component.ts
+++ src/app/pages/form/components/journal-statment/journal-statment.component.ts
@@ -160,11 +160,30 @@ export class JournalStatmentComponent {
 
       // Set the value of the edit form field, e.g., assigning to a variable or updating a form control
       this.editFormFieldValue = editFormFieldValue; // Update the value of editFormFieldValue with the actual form field variable
-      this.listJsForm.patchValue(rowData);
+      const formattedDate = this.formatDate(rowData.Journal_Date);
+      const matchingAccount = this.accounts.find(
+        (account) => account.Account_Name === rowData.Account_code
+      );
+      // Patch the values using a spread operator for clarity:
+      this.listJsForm.patchValue({
+        ...rowData,
+        Journal_Date: formattedDate,
+        Account_code: matchingAccount,
+       
+      });
       console.log(rowData); // Log the row data for verification
     } else {
       console.error("No data found for the specified ScheduleID");
     }
+  }
+
+  formatDate(dateString: string) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Add leading zero for single-digit months
+    const day = String(date.getDate()).padStart(2, '0');  // Add leading zero for single-digit days
+  
+    return `${year}-${month}-${day}`;
   }
   journal: any;
   deleteId: any;
